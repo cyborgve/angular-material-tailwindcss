4554d4b8dc2ed6dfafa9a429df3a5534400d098a
Richadr Iribarren <richadriribarren@gmail.com>
2024-06-12 13:02:22 -0400

    build: :building_construction: architectural changes like angular 18 style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import packageJson from '../../package.json';
+import { RouterOutlet } from '@angular/router';
+import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet],
 })
 export class AppComponent {
   title = 'angular-material-tailwindcss';
